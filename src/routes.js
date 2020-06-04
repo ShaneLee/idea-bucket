a368e06957643ed12b14a13c7a61206b8d3ecd2d
Shane Lee <me@shanel.ee>
2020-06-04 17:39:08 +0100

    Login working

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
-require("dotenv").config()
+require('dotenv').config()
+const passport = require('passport')
 
-const dbConnection = getDBConnection("idea_bucket")
+const dbConnection = getDBConnection('idea_bucket')
 
 function getDBConnection(database) {
   return mysql.createConnection({
@@ -15,102 +16,121 @@ function getDBConnection(database) {
   })
 }
 
-router.get("/", (req, res) => {
-  const queryString = "SELECT * FROM categories"
+router.get('/', (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.redirect('/login')
+  }
+  const queryString = 'SELECT * FROM categories'
   dbConnection.query(queryString, (err, rows, fields) => {
     if (err) {
-      console.log("Failed to query for /: " + err)
+      console.log('Failed to query for /: ' + err)
     }
-    console.log("Getting data from database for /")
-  res.render("index", { categories: rows })
+    console.log('Getting data from database for /')
+  res.render('index', { categories: rows })
   })
 })
 
-router.get("/ideas", (req, res) => {
-  const queryString = "SELECT * FROM ideas"
+router.get('/login', (req, res) => {
+    res.render('./pages/login')
+})
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+          if(info) {return res.send(info.message)}
+          if (err) { return next(err) }
+          if (!user) { return res.redirect('/login') }
+          req.login(user, (err) => {
+                  if (err) { return next(err); }
+                  return res.redirect('/')
+                })
+        })(req, res, next)
+})
+
+router.get('/ideas', (req, res) => {
+  const queryString = 'SELECT * FROM ideas'
   dbConnection.query(queryString, (err, rows, fields) => {
     if (err) {
-      console.log("Failed to query for /get_ideas: " + err)
+      console.log('Failed to query for /get_ideas: ' + err)
     }
-    console.log("Getting data from database for /get_ideas")
-    res.render("./pages/ideas", { ideas: rows })
+    console.log('Getting data from database for /get_ideas')
+    res.render('./pages/ideas', { ideas: rows })
   })
 
 })
 
-router.get("/category", (req, res) => {
-  res.render("./pages/category")
+router.get('/category', (req, res) => {
+  res.render('./pages/category')
 })
 
 
-router.post("/submit_idea", (req, res) => {
+router.post('/submit_idea', (req, res) => {
   idea = req.body.idea
   category = req.body.category
 
-  queryString = "INSERT INTO ideas (idea, category) \
-  VALUES (?, ?)"
+  queryString = 'INSERT INTO ideas (idea, category) \
+  VALUES (?, ?)'
   dbConnection.query(queryString, [idea, category], (err, results, field) => {
     if (err) {
-      console.log("Failed to submit idea. " + err)
+      console.log('Failed to submit idea. ' + err)
       return
     }
-    console.log("Logged new idea " + results)
+    console.log('Logged new idea ' + results)
   })
-  res.redirect("/")
+  res.redirect('/')
 })
 
-router.post("/submit_category", (req, res) => {
+router.post('/submit_category', (req, res) => {
   category = req.body.category
 
-  queryString = "INSERT INTO categories (category) \
-  VALUES (?)"
+  queryString = 'INSERT INTO categories (category) \
+  VALUES (?)'
   dbConnection.query(queryString, [category], (err, results, field) => {
     if (err) {
-      console.log("Failed to submit category. " + err)
+      console.log('Failed to submit category. ' + err)
       return
     }
-    console.log("Logged new category " + results)
+    console.log('Logged new category ' + results)
   })
-  res.redirect("/")
+  res.redirect('/')
 })
 
-router.post("/delete/:ideas_id", (req, res) => {
+router.post('/delete/:ideas_id', (req, res) => {
   const ideas_id = req.params.ideas_id
-  queryString = "DELETE from ideas WHERE ideas_id = ?"
+  queryString = 'DELETE from ideas WHERE ideas_id = ?'
   dbConnection.query(queryString, [ideas_id], (err, results, field) => {
     if (err) {
-      console.log("Failed to delete idea " + err)
+      console.log('Failed to delete idea ' + err)
       return
     }
-    console.log("Deleted idea" + results)
+    console.log('Deleted idea' + results)
   })
-  res.redirect("/ideas")
+  res.redirect('/ideas')
 
 })
 
-router.post("/api/submit_idea", (req, res) => {
+router.post('/api/submit_idea', (req, res) => {
   idea = req.body.idea
   category = req.body.category
 
-  queryString = "INSERT INTO ideas (idea, category) \
-  VALUES (?, ?)"
+  queryString = 'INSERT INTO ideas (idea, category) \
+  VALUES (?, ?)'
   dbConnection.query(queryString, [idea, category], (err, results, field) => {
     if (err) {
-      console.log("Failed to submit idea. " + err)
+      console.log('Failed to submit idea. ' + err)
       return
     }
-    console.log("Logged new idea " + results)
+    console.log('Logged new idea ' + results)
   })
-  res.send("Great Success")
+  res.send('Great Success')
 })
 
-router.get("/api/ideas", (req, res) => {
-  const queryString = "SELECT * FROM ideas"
+router.get('/api/ideas', (req, res) => {
+  const queryString = 'SELECT * FROM ideas'
   dbConnection.query(queryString, (err, rows, fields) => {
     if (err) {
-      console.log("Failed to query for /get_ideas: " + err)
+      console.log('Failed to query for /get_ideas: ' + err)
     }
-    console.log("Getting data from database for /get_ideas")
+    console.log('Getting data from database for /get_ideas')
     res.send({ ideas: rows })
   })
 
