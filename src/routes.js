1551f8f8cb84edecd19efb0443b9c6a4ebf92494
Shane Lee <me@shanel.ee>
2020-07-07 18:54:55 +0100

    Revert "Refactor to database class"
    This reverts commit 445dcfbeded22a820ef74534d9cb5598ab73bde8.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mysql = require('mysql')
 const router = express.Router()
 require('dotenv').config()
 const passport = require('passport')
-const db = require('./database')
+
+const dbConnection = getDBConnection('idea_bucket')
+
+function getDBConnection(database) {
+  return mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: database
+  })
+}
 
 const checkLoggedIn = (req, res) => {
   if (!req.isAuthenticated()) {
@@ -12,7 +24,14 @@ const checkLoggedIn = (req, res) => {
 
 router.get('/', (req, res) => {
   checkLoggedIn(req, res)
-  res.render('index', { categories: db.getCategories() })
+  const queryString = 'SELECT * FROM categories'
+  dbConnection.query(queryString, (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to query for /: ' + err)
+    }
+    console.log('Getting data from database for /')
+  res.render('index', { categories: rows })
+  })
 })
 
 router.get('/login', (req, res) => {
@@ -33,7 +52,15 @@ router.post('/login', (req, res, next) => {
 
 router.get('/ideas', (req, res) => {
   checkLoggedIn(req, res)
-  res.render('./pages/ideas', { ideas: db.getIdeas() })
+  const queryString = 'SELECT * FROM ideas'
+  dbConnection.query(queryString, (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to query for /get_ideas: ' + err)
+    }
+    console.log('Getting data from database for /get_ideas')
+    res.render('./pages/ideas', { ideas: rows })
+  })
+
 })
 
 router.get('/category', (req, res) => {
@@ -44,33 +71,80 @@ router.get('/category', (req, res) => {
 
 router.post('/submit_idea', (req, res) => {
   checkLoggedIn(req, res)
-  db.submitIdea(req.body.idea, req.body.category)
+  idea = req.body.idea
+  category = req.body.category
+
+  queryString = 'INSERT INTO ideas (idea, category) \
+  VALUES (?, ?)'
+  dbConnection.query(queryString, [idea, category], (err, results, field) => {
+    if (err) {
+      console.log('Failed to submit idea. ' + err)
+      return
+    }
+    console.log('Logged new idea ' + results)
+  })
   res.redirect('/')
 })
 
 router.post('/submit_category', (req, res) => {
   checkLoggedIn(req, res)
   category = req.body.category
+
+  queryString = 'INSERT INTO categories (category) \
+  VALUES (?)'
+  dbConnection.query(queryString, [category], (err, results, field) => {
+    if (err) {
+      console.log('Failed to submit category. ' + err)
+      return
+    }
+    console.log('Logged new category ' + results)
+  })
   res.redirect('/')
 })
 
 router.post('/delete/:ideas_id', (req, res) => {
   checkLoggedIn(req, res)
-  db.deleteIdea(req.params.ideas_id)
+  const ideas_id = req.params.ideas_id
+  queryString = 'DELETE from ideas WHERE ideas_id = ?'
+  dbConnection.query(queryString, [ideas_id], (err, results, field) => {
+    if (err) {
+      console.log('Failed to delete idea ' + err)
+      return
+    }
+    console.log('Deleted idea' + results)
+  })
   res.redirect('/ideas')
 
 })
 
 router.post('/api/submit_idea', (req, res) => {
   checkLoggedIn(req, res)
-  if (db.submitIdea(req.body.idea, req.body.category)) {
-    res.send('Great Success')
-  }
+  idea = req.body.idea
+  category = req.body.category
+
+  queryString = 'INSERT INTO ideas (idea, category) \
+  VALUES (?, ?)'
+  dbConnection.query(queryString, [idea, category], (err, results, field) => {
+    if (err) {
+      console.log('Failed to submit idea. ' + err)
+      return
+    }
+    console.log('Logged new idea ' + results)
+  })
+  res.send('Great Success')
 })
 
 router.get('/api/ideas', (req, res) => {
   checkLoggedIn(req, res)
-  res.send({ ideas: db.getIdeas() })
+  const queryString = 'SELECT * FROM ideas'
+  dbConnection.query(queryString, (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to query for /get_ideas: ' + err)
+    }
+    console.log('Getting data from database for /get_ideas')
+    res.send({ ideas: rows })
+  })
+
 })
 
 module.exports = router
